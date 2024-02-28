453a2f82df999fd6f617e45987b5572c11bb77f6
Larry Wisniewski <larryw48390@gmail.com>
2024-02-28 07:01:22 -0500

    finished save screen and end screen, finished highscores

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,12 @@ let quizContainer = document.querySelector(".quizcontainer");
 
 let hsInitials = "";
 let timer = 40;
+let finalTime;
 let correctCount = 0;
 let incorrectCount = 0;
+let scores = [];
+let endGame = false;
+let hsLength = 5;
 
 const questionsDB = [["What scope do variables have in javascript?", "global, block", "local, block", "global, local", "all scopes"], ["What does a loop do?", "Selects all variables.", "Loops through an arrau", "Causes an infinite loop error", "Executes code multiple times"], ["What event handler handles when a button is clicked?", "submit", "click", "keydown", "change"], ["Local storage values are always stored as a(n) ___", "string", "variable", "integer", "boolean"], ["console.log will:", "log errors in your code.", "print something to the console", "tell the console to logout", "log all variables to console"]];
 
@@ -13,7 +17,12 @@ let questionAmount = questionsDB.length
 
 const correctDB = ["global, local", "Executes code multiple times", "click", "string", "print something to the console"];
 
-
+function init() {
+    let storedScores = JSON.parse(localStorage.getItem("scores"))
+    if (storedScores !== null) {
+        scores = storedScores;
+    };
+}
 
 function startQuiz() {
     if (initialsText.value === "") {
@@ -58,6 +67,79 @@ function endQuiz() {
     });
 };
 
+function saveHighScore() {
+    endGame = true;
+    if (incorrectCount === questionAmount) {
+        let gameoverText = document.createElement("h2");
+        let playAgainBtn = document.createElement("button");
+        gameoverText.setAttribute("id", "questiontext");
+        quizContainer.appendChild(gameoverText);
+        quizContainer.appendChild(playAgainBtn);
+        gameoverText.textContent = "Game Over! You answered 0 correctly. Play Again?";
+        playAgainBtn.innerHTML = "Play Again";
+        playAgainBtn.addEventListener("click", function() {
+            location.reload();
+            return false;
+        });
+    } else {
+        let highScore = {
+            name: hsInitials,
+            time: finalTime,
+        };
+        scores.push(highScore);
+        let saveText = document.createElement("h2");
+        let saveBtn = document.createElement("button");
+        let playAgainBtn = document.createElement("button");
+        quizContainer.appendChild(saveText);
+        quizContainer.appendChild(saveBtn);
+        quizContainer.appendChild(playAgainBtn);
+        saveText.textContent = "Great Job! You got " + correctCount + " questions correct and " + incorrectCount + " questions incorrect. Would you like to save your score?";
+        saveBtn.innerHTML = "Save Score";
+        playAgainBtn.innerHTML = "Play Again";
+        saveBtn.addEventListener("click", function() {
+            localStorage.setItem("scores", JSON.stringify(scores));
+            renderScores(saveBtn, saveText);
+        });
+        playAgainBtn.addEventListener("click", function() {
+            location.reload();
+            return false;
+        });
+    };
+};
+
+function renderScores(btn, text) {
+    btn.remove();
+    text.remove();
+    let scoreUL = document.createElement("ol");
+    quizContainer.appendChild(scoreUL);
+    let clearBtn = document.createElement("button");
+    clearBtn.textContent = "Clear Scores"
+    quizContainer.appendChild(clearBtn);
+    scores = scores.sort((a, b) => b.time - a.time);
+    console.log(scores);
+    for (let i = 0; i < hsLength; i++) {
+        var name = scores[i].name;
+        console.log(name);
+        var score = scores[i].time;
+        var li = document.createElement("li");
+        li.textContent = "Intials: " + name + " Score: " + score;
+        scoreUL.appendChild(li)
+    };
+    clearBtn.addEventListener("click", clearScores);
+};
+
+function clearScores(event) {
+    btn = event.target;
+    let ol = document.querySelector("ol");
+    let li = document.querySelectorAll("li");
+    ol.remove();
+    for (let i = 0; i < li.length; i++) {
+        li[i].remove();
+    };
+    localStorage.removeItem("scores")
+    btn.remove();
+}
+
 function startTimer() {
     let timerText = document.createElement("h2");
     quizContainer.appendChild(timerText);
@@ -71,11 +153,16 @@ function startTimer() {
             endQuiz();
             clearInterval(timerInterval);
         };
+        if (endGame === true) {
+        clearInterval(timerInterval);
+        };
     }, 1000);
 };
 
 function selectQuestion() {
     if (questionsDB.length === 0) {
+        let timerText = document.querySelector("#timerh2");
+        finalTime = timer;
         timerText.remove();
         saveHighScore();
         return;
@@ -121,7 +208,7 @@ function validateAnswers(event) {
         correctText.setAttribute("id", "outcome");
         correctText.textContent = "Correct!";
         correctText.style.color = "green";
-        correctCount = correctCount++
+        correctCount = correctCount + 1;
         deleteOutcomeText();
         removeQuestion();
     } else {
@@ -130,8 +217,8 @@ function validateAnswers(event) {
         incorrectText.setAttribute("id", "outcome");
         incorrectText.textContent = "Incorrect!";
         incorrectText.style.color = "red";
-        timer = timer - 5
-        incorrectCount = incorrectCount++
+        timer = timer - 5;
+        incorrectCount = incorrectCount + 1;
         deleteOutcomeText();
         removeQuestion();
     };
@@ -163,5 +250,5 @@ function removeQuestion() {
     selectQuestion();
 };
 
-
+init();
 playBtn.addEventListener("click", startQuiz)
